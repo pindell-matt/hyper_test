bd84097bb0c42c9c264bed2c5a5cfabbcaeba466
Matt Pindell <pindell.m@gmail.com>
2016-04-16 15:01:07 -0600

    ARGV get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate hyper;
+
+use std::io::Read;
+use std::env;
+
+use hyper::Client;
+use hyper::header::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+
+    let url: String = args[1].to_owned();
+    let url_slice: &str = &url[..];
+
+    let mut client = Client::new();
+
+    let mut res = client.get(url_slice)
+        .header(Connection::close())
+        .send().unwrap();
+
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    println!("Response: {}", body);
 }
